02bda093b6e4419d6479fa671b27f70b689dd4f1
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-04 23:12:35 +0530

    SoundSection

---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function SoundSection() {
+    return ( 
+        <div className="sound-section wrapper">
+            <div className="body">
+            <div className="sound-section-content content">
+                <h2 className="title">New Sound System</h2>
+                <p className="text">Teel the base</p>
+                <span className="desc">From $41.62/mo. or $999 before trade-in</span>
+            </div>
+            </div>
+        </div>
+     );
+}
+
+export default SoundSection;
