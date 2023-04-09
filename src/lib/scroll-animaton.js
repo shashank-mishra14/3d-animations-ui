6cd38959de8483421cdf84f456f4fcd75b117b88
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-09 18:00:48 +0530

    animation

--- src/lib/scroll-animaton.js
+++ src/lib/scroll-animaton.js
@@ -4,18 +4,48 @@ export const  scrollAnimation =(position, target, onUpdate) => {
     const tl = gsap.timeline();
 
     tl.to(position,{                    // the "to" method tell us the parameters that are going to be changed.//
-        x,
-        y,
-        z,    
+        x: -3.38,
+        y:-10.74,
+        z:-5.93,    
         scrollTrigger:{
             trigger:' .sound-section',
             start: "top bottom",
             end:"top top",
             scrub:2,
             immediateRender:false
-        }
-
+        },
         onUpdate
-
+    })
+    .to(target,{                    // the "to" method tell us the parameters that are going to be changed.//
+        x: 1.52,
+        y:0.77,
+        z:-1.08,    
+        scrollTrigger:{
+            trigger:' .sound-section',
+            start: "top bottom",
+            end:"top top",
+            scrub:2,
+            immediateRender:false
+        },
+    })
+    .to('.jumbotron-section ',{                    // the "to" method tell us the parameters that are going to be changed.//
+        opacity:0,  
+        scrollTrigger:{
+            trigger:' .sound-section',
+            start: "top bottom",
+            end:"top top",
+            scrub:2,
+            immediateRender:false
+        },
+    })
+    .to('.sound-section-content ',{                    // the "to" method tell us the parameters that are going to be changed.//
+        opacity:1,  
+        scrollTrigger:{
+            trigger:' .sound-section',
+            start: "top bottom",
+            end:"top top",
+            scrub:2,
+            immediateRender:false
+        },
     })
 }
