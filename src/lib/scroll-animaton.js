34ab20678045b81b1afeb959cea289312f49f7ec
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-09 17:51:05 +0530

    changes

--- src/lib/scroll-animaton.js
+++ src/lib/scroll-animaton.js
@@ -2,4 +2,20 @@ import gsap  from "gsap";
 
 export const  scrollAnimation =(position, target, onUpdate) => {
     const tl = gsap.timeline();
+
+    tl.to(position,{                    // the "to" method tell us the parameters that are going to be changed.//
+        x,
+        y,
+        z,    
+        scrollTrigger:{
+            trigger:' .sound-section',
+            start: "top bottom",
+            end:"top top",
+            scrub:2,
+            immediateRender:false
+        }
+
+        onUpdate
+
+    })
 }
