ac6b37862646095e4e1f513f7fad7e48a37eef8f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-10 23:08:08 +0530

    changes made to soundsection

--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function SoundSection() {
+
+    const handleLearnMore =() =>{
+        const element = document.querySelector(".display-section")
+        window.scrollTo({
+            top:element?.getBoundingClientRect().bottom,
+            left:0,
+            behavior:"smooth"
+        })
+    }
+
     return ( 
         <div className="sound-section wrapper">
             <div className="body">
@@ -13,7 +23,7 @@ function SoundSection() {
                         <button className="button">Buy</button>
                     </li>
                     <li>
-                        <a className="link">Learn more</a>
+                        <a className="link" onClick={handleLearnMore}>Learn more</a>
                     </li>
                 </ul>
             </div>
