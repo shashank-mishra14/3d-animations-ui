5402c070f6cf43276d338d4b6dabd40d77cbb2dc
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-05-03 23:19:02 +0530

    Top scroll position

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 function DisplaySection() {
+
+    const handleScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+            left: 0,
+        });
     return ( 
         <div className="display-section wrapper">
             <h2 className="title">New</h2>
@@ -9,7 +16,7 @@ function DisplaySection() {
                 A display that's upto 2x brighter in the sun.
             </span>
             <button className="button">Try me!</button>
-            <button className="back-button">TOP</button>
+            <button className="back-button"onClick={handleScrollToTop}>TOP</button>
         </div>
     );
 }
